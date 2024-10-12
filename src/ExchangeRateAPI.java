94bcad9258c2d9576e6160b0de2e422c34156113
YuliaGV <ygaviria7@gmail.com>
2024-10-12 15:01:18 -0500

    include-api-result

--- src/ExchangeRateAPI.java
+++ src/ExchangeRateAPI.java
@@ -1,9 +1,37 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ExchangeRateAPI {
 
     private static final String API_URL = "https://api.exchangerate-api.com/v4/latest/";
 
     public static double getExchangeRate(String fromCurrency, String toCurrency) throws Exception {
-        return 1.5;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request  = HttpRequest.newBuilder()
+                .uri(URI.create(API_URL+fromCurrency))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String jsonData = response.body();
+
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+
+        JsonObject jsonObject = gson.fromJson(jsonData, JsonObject.class);
+
+        JsonObject conversionRatesObject = jsonObject.getAsJsonObject("rates");
+        return conversionRatesObject.get(toCurrency).getAsDouble();
+
+
     }
 
 }
